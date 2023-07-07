c05a06915bb4fdbd498b88d0b819d91b619f8813
GamerderMacht <dougkan.mountour@gmail.com>
2023-07-07 23:23:28 +0200

    Movement

--- Assets/Scripts/KeyboardController.cs
+++ Assets/Scripts/KeyboardController.cs
@@ -9,12 +9,37 @@ public class KeyboardController : Controller
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveObject();
+    }
+
+    private void MoveObject()
+    {
+        transform.position = ReturnMousePosition();
+        transform.position = new Vector3 (Mathf.Clamp(transform.position.x, -constrainX, constrainX),
+        0f,
+        Mathf.Clamp(transform.position.z, -constrainZ, constrainZ));       
+
+        // Debug.DrawRay(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.down * 10, Color.red, 10); 
+    }
+
+    private Vector3 ReturnMousePosition()
+    {
+        Vector3 result = new Vector3(0,0,0); 
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider != null)
+            {
+                result = hit.point;
+            }
+        }
+                
+        return result;
     }
 }
